6335ab92d1a6c6104616e63175277dea8d20f684
Luca Perali <luca.perali@studenti.unipd.it>
2019-04-12 15:28:52 +0200

    calibrate camera, set the same objectPoints for all images, should be specific

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -36,21 +36,71 @@ int main(int argc, char** argv) {
 
     //check params
     if (argc != 2) {
-        cout << "usage: ./lab2 <path_of_folder_within_images>" << endl;
+        cout << "usage: ./lab2 <path_of_directory_within_images>" << endl;
         return EXIT_FAILURE;
     }
+
+    // check if dir exists else
     const cv::String path(argc>1 ? argv[1] : ".");    // path of directory with images
     if (!cv::utils::fs::exists(path)) {
         cout << "specified path does not exist: " << path << endl;
+        return EXIT_FAILURE;
+    } else {
+        cout << "looking for images in " << path << endl;
     }
     vector<cv::String> fn;                      // path list
-    cv::glob(path,fn,false);              // get files list false1 = recursive, false2 = include directory
-    vector<vector<Point2f>> corners;            // this will be filled by the detected corners
+    cv::glob(path,fn,false);                    // get files list false = recursive,
+    int numBoards = fn.size();                  // number of boards is the number of file in the specified dir
+    int numCornersHor;                          // number of corners per column
+    int numCornersVer;                          // number of corners per row
 
-    // const boost::filesystem::path p(argc>1 ? argv[1] : ".");  // avoid repeated path construction below
-    for (int i = 0; i < fn.size(); i++) {
+    // get input params
+    printf("Enter number of corners along width: ");
+    scanf("%d", &numCornersHor);
+
+    printf("Enter number of corners along height: ");
+    scanf("%d", &numCornersVer);
+
+    int numSquares = numCornersHor * numCornersVer; // number of corners in each images
+
+    vector<vector<Point2f>> corners;            // this will be filled by the detected corners
+    vector<vector<Point3f>> objectPoints;       // 3D relative position of the points in the space
+    vector<Size> imgs_size;                     // size of the images
+
+    // set relative points position in the space.
+    vector<Point3f> obj;
+    for(int j=0;j<numSquares;j++)
+        obj.push_back(Point3f(j/numCornersHor, j%numCornersHor, 0.0f));
+
+    /**
+     * for each image, save:
+     * the relative point position in the space,
+     * the founded corners,
+     * the image size.
+    */
+    for (int i = 0; i < numBoards; i++) {
+        objectPoints.push_back(obj);
         corners.push_back(get_corners(fn[i]));
+        Mat img = cv::imread(fn[i]);
+        imgs_size.push_back(img.size());
     }
-    
+
+    // calibrate camera
+    Mat intrinsic = Mat(3, 3, CV_32FC1); // filled by calibrateCamera, rapresents intrinsic parameters 
+    intrinsic.ptr<float>(0)[0] = 1;      // initialization value if no changes are required
+    intrinsic.ptr<float>(1)[1] = 1;
+
+    Mat distCoeffs = Mat::zeros(8, 1, CV_64F);      // filled by calibrateCamera, 
+    Size board_sz = Size(numCornersHor, numCornersVer); // size of the board
+    vector<Mat> rvecs;          // Output vector of rotation vectors estimated for each pattern view
+    vector<Mat> tvecs;          // Output vector of translation vectors estimated for each pattern view
+
+    double rms = cv::calibrateCamera(objectPoints, corners, imgs_size[0], intrinsic, distCoeffs, rvecs, tvecs);
+
+    // print  the output
+    cout << "calibrateCamera return: " << rms << endl;
+    cout << "intrinsicFactor = "<< endl << " "  << intrinsic << endl << endl;
+    cout << "distCoeffs = "<< endl << " "  << distCoeffs << endl << endl;
+
 }
-// cmake . && make && ./lab2 ./D3300/img10.jpg
+// cmake . && make && ./lab2 D3300 >> D3300_ouput.txt 
